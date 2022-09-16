6579c4faffcb9793c912dcd540833fe27d50b14a
echo-lxq <echo_lxq@163.com>
2022-09-16 10:49:39 +0800

    2022/09/16

--- src/08-antd-mobile/views/films/NowPlaying.js
+++ src/08-antd-mobile/views/films/NowPlaying.js
@@ -2,76 +2,114 @@
  * @Author: WeiShan
  * @Date: 2022-09-06 11:03:08
  * @LastEditors: WeiShan
- * @LastEditTime: 2022-09-08 22:01:52
- * @FilePath: \react-learn\src\04-router\views\films\NowPlaying.js
+ * @LastEditTime: 2022-09-16 10:36:27
+ * @FilePath: \react-learn\src\08-antd-mobile\views\films\NowPlaying.js
  * @Description: 
  * 
  * Copyright (c) 2022 by WeiShan/xls, All Rights Reserved. 
  */
 import React from 'react'
 import axios from 'axios'
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 import { useState } from 'react'
-import { withRouter } from 'react-router-dom'
+// import { withRouter } from 'react-router-dom'
+import { List , Image , InfiniteScroll } from 'antd-mobile'
+import { useRef } from 'react'
 
 export default function NowPlaying(props) {
   const [list,setList] = useState([])
-  useEffect(()=>{
-    axios({
-      url:"/gateway?cityId=110100&pageNum=1&pageSize=10&type=1&k=3197837",
-      headers:{
-        'X-Client-Info':'{"a":"3000","ch":"1002","v":"5.2.1","e":"16625201711699604458373121","bc":"110100"}',
-        'X-Host': 'mall.film-ticket.film.list'
-      }
-    }).then(res=>{
-      setList(res.data.data.films)
-      console.log(res.data.data.films)
-    }).catch(err=>{
-      console.log(err)
-    })
-    // return()=>{
-    //   console.log("组件销毁")
-    //   // window.location.href=''
-    // }
-  },[])
+  const [hasMore,setHasMore] = useState(true)
+
+  const count = useRef(0)
+
+  // useEffect(()=>{
+  //   axios({
+  //     url:"/gateway?cityId=110100&pageNum=1&pageSize=10&type=1&k=3197837",
+  //     headers:{
+  //       'X-Client-Info':'{"a":"3000","ch":"1002","v":"5.2.1","e":"16625201711699604458373121","bc":"110100"}',
+  //       'X-Host': 'mall.film-ticket.film.list'
+  //     }
+  //   }).then(res=>{
+  //     setList(res.data.data.films)
+  //     console.log(res.data.data.films)
+  //   }).catch(err=>{
+  //     console.log(err)
+  //   })
+  // },[])
   
-  // const handleChangePage = (id)=>{
-  //   // console.log("click")
-    
-  //   //原生js方法
-  //   // window.location.href = "#/detail/"+id
-    
-  //   //当前组件包含在Route中，为Route子组件，可以接受props
+  const loadMore = ()=>{
+      count.current++
 
-  //   // 1.动态路由传参
-  //   // props.history.push(`/detail/${id}`)
+      setHasMore(false)
 
-  //   // 2. query传参
-  //   // props.history.push({pathname:'/detail',query:{id:id}})
+      console.log("到底")
 
-  //   // 3. state传参
-  //   props.history.push({pathname:'/detail',state:{id:id}})
+      axios({
+        url:`/gateway?cityId=110100&pageNum=${count.current}&pageSize=10&type=1&k=3197837`,
+        headers:{
+          'X-Client-Info':'{"a":"3000","ch":"1002","v":"5.2.1","e":"16625201711699604458373121","bc":"110100"}',
+          'X-Host': 'mall.film-ticket.film.list'
+        }
+      }).then(res=>{
+        setList([...list,...res.data.data.films])
+      setHasMore(res.data.data.films.length>0)
+        console.log(res.data.data.films)
+      }).catch(err=>{
+        console.log(err)
+      })
 
-  // }
+  }
+  
   return (
     <div>
-      {list.map(item=><WithFilmItem key={item.filmId} {...item}></WithFilmItem>)}
+      <List>
+        {list.map(item=>
+          <List.Item
+            key={item.filmId} {...item}
+            prefix = {
+              <Image 
+                src={item.poster}
+                // style={{borderRadius:20}}
+                // fit='cover'
+                width={80}
+                // height={40}
+              />
+            }
+            description={<div>
+              {
+                item.grade?<div>观众评分：{item.grade}</div>:
+                <div style={{visibility:"hidden"}}>观众评分：{item.grade}</div>
+              }
+              
+              <div>主演：***</div>
+              <div>{item.nation}</div>
+              <div>{item.runtime}分钟</div>
+              
+            </div>}
+            onClick={()=>{
+              // console.log(filmId)
+              props.history.push({pathname:'/detail',state:{id:item.filmId}})
+            }}
+          >
+              {item.name}
+          </List.Item>
+        )}
+      </List>
+      <InfiniteScroll loadMore={loadMore} hasMore={hasMore} />
     </div>
   )
 }
 
-function FilmItem(props){
-  // const item = props.item
-  let {name,filmId,poster} = props
-  return(
-    <li onClick={()=>{
-      // handleChangePage(item.filmId)
-      console.log(filmId)
-      props.history.push({pathname:'/detail',state:{id:filmId}})
-    }}>
-      <img src={poster} alt={name} style={{height:"80px"}} />
-      {name}</li>
-  )
-}
+// function FilmItem(props){
+//   let {name,filmId,poster} = props
+//   return(
+    // <li onClick={()=>{
+    //   console.log(filmId)
+    //   props.history.push({pathname:'/detail',state:{id:filmId}})
+    // }}>
+//       <img src={poster} alt={name} style={{height:"80px"}} />
+//       {name}</li>
+//   )
+// }
 
-const WithFilmItem = withRouter(FilmItem)
+// const WithFilmItem = withRouter(FilmItem)
